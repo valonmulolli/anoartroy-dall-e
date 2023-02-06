0e93123d5ce24e0644dcb5ad4974b0a1d904ae8b
valon <valonmulolli@gmail.com>
2023-02-06 17:42:41 +0100

    update fetch url

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 		setloading(true);
 
 		try {
-			const response = await fetch("http://localhost:8080/api/v1/post", {
+			const response = await fetch("https://anoartroy-dall-e.onrender.com", {
 				method: "GET",
 				headers: {
 					"Content-Type": "application/json",
