6cb58915565ecbe239e0406f60aa5aa2f2aceb08
valon <valonmulolli@gmail.com>
2023-02-08 16:17:18 +0100

    mongodb update

--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,21 +1,11 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-	try {
-		mongoose.set("strictQuery", true);
-		mongoose
-			.connect(process.env.MONGO_URL, {
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			})
-			.then(() => {
-				console.log("Connection Successful");
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	} catch (error) {
-		console.log(error);
-	}
-};
+import mongoose from 'mongoose'
 
-module.exports = connectDB;
+const connectDB =(uri) => {
+  mongoose.set('strictQuery', true);
+
+  mongoose.connect(uri)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log(err))
+}
+
+export default connectDB;
--- server/mongodb/models/post.js
+++ server/mongodb/models/post.js
@@ -1,18 +1,11 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-	},
-	prompt: {
-		type: String,
-		required: true,
-	},
-	photo: {
-		type: String,
-		required: true,
-	},
+	name: { type: String, required: true },
+	prompt: { type: String, required: true },
+	photo: { type: String, required: true },
 });
 
-module.exports = mongoose.model("Post", Post);
+const PostSchema = mongoose.model("Post", Post);
+
+export default PostSchema;
